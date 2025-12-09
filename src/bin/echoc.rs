6011a65069bc4cfcd966b5f39e0d1b8093d405a9
xinchen <xinchentechnote@163.com>
2025-12-09 22:09:19 +0800

    chore:fix log and shell script

--- src/bin/echoc.rs
+++ src/bin/echoc.rs
@@ -2,6 +2,7 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 
 use clap::Parser;
+use tracing::info;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -16,10 +17,10 @@ fn main() {
     let args = Args::parse();
     let target = format!("{}:{}", args.ip, args.port);
     let mut stream = TcpStream::connect(target.as_str()).unwrap();
-    println!("Connect to {}", target);
+    info!("Connect to {}", target);
     stream.write_all(b"hello world\n").unwrap();
 
     let mut buf = [0; 1024];
     let n = stream.read(&mut buf).unwrap();
-    println!("client received: {}", String::from_utf8_lossy(&buf[..n]));
+    info!("Client received: {}", String::from_utf8_lossy(&buf[..n]));
 }
--- src/bin/echos.rs
+++ src/bin/echos.rs
@@ -2,6 +2,7 @@ use std::io::{Read, Write};
 use std::net::TcpListener;
 
 use clap::Parser;
+use tracing::info;
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
@@ -14,12 +15,13 @@ fn main() {
     let args = Args::parse();
     let target = format!("{}:{}", args.host, args.port);
     let listener = TcpListener::bind(target.as_str()).unwrap();
-    println!("server listening on {}", target);
+    info!("server listening on {}", target);
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         let mut buf = [0; 1024];
         let n = stream.read(&mut buf).unwrap();
+        info!("Server received: {}", String::from_utf8_lossy(&buf[..n]));
         stream.write_all(&buf[..n]).unwrap();
     }
 }
